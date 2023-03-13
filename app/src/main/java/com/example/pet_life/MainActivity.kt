60e4bd5b0422b2dff4d9b48335e02fbdcccf78a0
Viktor <viktor.taranau@gmail.com>
2023-03-13 21:18:12 +0300

    add button
    Signed-off-by: Viktor <viktor.taranau@gmail.com>

--- app/src/main/java/com/example/pet_life/MainActivity.kt
+++ app/src/main/java/com/example/pet_life/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.pet_life
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.os.Bundle
-
-import android.annotation.SuppressLint //quick fix "Local variable name 'my_image' should not contain underscores"
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import android.util.Log
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,12 +22,26 @@ class MainActivity : AppCompatActivity() {
         val my_image = findViewById<FloatingActionButton>(R.id.fab)
         my_image.setOnTouchListener(object : View.OnTouchListener {
             override fun onTouch(v: View, ev: MotionEvent): Boolean {
-                // Perform tasks here
-
-                //mScaleDetector.onTouchEvent(ev)
-                Log.d("TACH", "message!!!!!!!!!!!!!!!!")
-
-
+                //Log.d("TACH", "TACH")
+                when (ev.action){
+                    MotionEvent.ACTION_DOWN or MotionEvent.ACTION_MOVE -> {
+                        val screenX: Float = v.x + ev.getX()
+                        val screenY: Float = v.y + ev.getY()
+                        //Log.d("TACH", String.format(Locale.US, "screenX = %f", screenX))
+                        my_image.translationX = screenX - my_image.width/2
+                        my_image.translationY = screenY - my_image.height/2
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        ObjectAnimator.ofFloat(my_image, "translationX", (my_image.width).toFloat()).apply {
+                            duration = 200
+                            start()
+                        }
+                        ObjectAnimator.ofFloat(my_image, "translationY", (my_image.height).toFloat()).apply {
+                            duration = 200
+                            start()
+                        }
+                    }
+                }
                 return true
             }
         })
